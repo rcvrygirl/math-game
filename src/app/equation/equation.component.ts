624b384c1d7506eb96758e6aa0ea9b1804776504
Calista Pemberton <minervamachina@gmail.com>
2022-05-06 12:09:25 +0800

    add form validations

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
@@ -12,9 +12,25 @@ export class EquationComponent implements OnInit {
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
     answer: new FormControl('')
-  })
+  }, [
+    (form: AbstractControl) => {
+      const { a, b, answer } = form.value;
+      if (a + b === parseInt(answer)) {
+        return null;
+      }
+      return { addition: true };
+    }
+  ])
 
   constructor() { }
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
 
   ngOnInit(): void {
   }
